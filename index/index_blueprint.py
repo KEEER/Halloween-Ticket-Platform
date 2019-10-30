01acf5a0a61c48b09c28880747eb4ccbebc512f9
QueueHuo <queuehuo@gmail.com>
2019-10-30 14:29:49 +0800

    fix: add debug mark.

--- index/index_blueprint.py
+++ index/index_blueprint.py
@@ -3,6 +3,9 @@ from consts import ROUTES
 from data import data_manager
 index_blueprint = Blueprint('index_blueprint', __name__)
 
+import logging 
+logger = logging.getLogger('index_logger')
+
 @index_blueprint.route(ROUTES.INDEX, methods = ['GET'])
 def process_index():
     token = request.cookies.get('kas-account-token','')
@@ -13,5 +16,6 @@ def process_index():
         user_info = data_manager.get_user_informtaion(token)
         return render_template('index.html', NICKNAME = user_info['nickname'])
     except Exception as e:
+        logger.exception('Error %s'%str(e))
         print('ERROR: %s'%str(e))
         return redirect('/login')
